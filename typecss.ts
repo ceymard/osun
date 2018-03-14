d57a6a1791178575fd82aca2db8b510e00f420c8
Christophe Eymard <christophe.eymard@sales-way.com>
2018-03-14 14:42:51 +0100

    s can now receive a TemplateStringArray

--- typecss.ts
+++ typecss.ts
@@ -168,9 +168,10 @@ export const all = new Selector('*')
 export const empty = new Selector('') // empty should not be defined
 
 
-export function s(sel: string | Selector) {
-  return sel instanceof Selector ? sel : new Selector(sel)
+export function s(sel: string | Selector | TemplateStringsArray) {
+  return sel instanceof Selector ? sel : new Selector(Array.isArray(sel) ? sel[0] : sel)
 }
+
 
 export function keyframes(name: string, keyframes: {[name: string]: CSSProperties}) {
   name = cls(name)
@@ -194,20 +195,3 @@ export function media(query: string, fn: () => void) {
 export function raw(css: string) {
   sheet.push(css)
 }
-
-
-export type ElementNames = 'a'|'abbr'|'acronym'|'address'|'applet'|'area'|'article'|'aside'|'audio'|'b'|'base'|'basefont'|'bdi'|'bdo'|'big'|'blockquote'|'body'|'br'|'button'|'canvas'|'caption'|'center'|'cite'|'code'|'col'|'colgroup'|'data'|'datalist'|'dd'|'del'|'details'|'dfn'|'dialog'|'dir'|'div'|'dl'|'dt'|'em'|'embed'|'fieldset'|'figcaption'|'figure'|'font'|'footer'|'form'|'frame'|'frameset'|'h1'|'h2'|'h3'|'h4'|'h5'|'h6'|'head'|'header'|'hgroup'|'hr'|'html'|'i'|'iframe'|'img'|'input'|'ins'|'isindex'|'kbd'|'keygen'|'label'|'legend'|'li'|'link'|'main'|'map'|'mark'|'menu'|'menuitem'|'meta'|'meter'|'nav'|'nextid'|'noframes'|'noscript'|'object'|'ol'|'optgroup'|'option'|'output'|'p'|'param'|'picture'|'plaintext'|'pre'|'progress'|'q'|'rb'|'rbc'|'rp'|'rt'|'rtc'|'ruby'|'s'|'samp'|'script'|'section'|'select'|'small'|'source'|'span'|'strike'|'strong'|'style'|'sub'|'summary'|'sup'|'table'|'tbody'|'td'|'template'|'textarea'|'tfoot'|'th'|'thead'|'time'|'title'|'tr'|'track'|'tt'|'u'|'ul'|'var'|'video'|'wbr'|'xmp'|'animate'|'animateMotion'|'animateTransform'|'circle'|'clipPath'|'cursor'|'defs'|'desc'|'discard'|'ellipse'|'feBlend'|'feColorMatrix'|'feComponentTransfer'|'feComposite'|'feConvolveMatrix'|'feDiffuseLighting'|'feDisplacementMap'|'feDistantLight'|'feDropShadow'|'feFlood'|'feFuncA'|'feFuncB'|'feFuncG'|'feFuncR'|'feGaussianBlur'|'feImage'|'feMerge'|'feMergeNode'|'feMorphology'|'feOffset'|'fePointLight'|'feSpecularLighting'|'feSpotLight'|'feTile'|'feTurbulence'|'filter'|'foreignObject'|'g'|'hatch'|'hatchpath'|'image'|'line'|'linearGradient'|'marker'|'mask'|'mesh'|'meshgradient'|'meshpatch'|'meshrow'|'metadata'|'mpath'|'path'|'pattern'|'polygon'|'polyline'|'radialGradient'|'rect'|'set'|'solidcolor'|'stop'|'svg'|'switch'|'symbol'|'text'|'textPath'|'tspan'|'unknown'|'use'|'view'
-const tags = [
-  // HTML Elements
-  'a','abbr','acronym','address','applet','area','article','aside','audio','b','base','basefont','bdi','bdo','big','blockquote','body','br','button','canvas','caption','center','cite','code','col','colgroup','data','datalist','dd','del','details','dfn','dialog','dir','div','dl','dt','em','embed','fieldset','figcaption','figure','font','footer','form','frame','frameset','h1','h2','h3','h4','h5','h6','head','header','hgroup','hr','html','i','iframe','img','input','ins','isindex','kbd','keygen','label','legend','li','link','main','map','mark','menu','menuitem','meta','meter','nav','nextid','noframes','noscript','object','ol','optgroup','option','output','p','param','picture','plaintext','pre','progress','q','rb','rbc','rp','rt','rtc','ruby','s','samp','script','section','select','small','source','span','strike','strong','style','sub','summary','sup','table','tbody','td','template','textarea','tfoot','th','thead','time','title','tr','track','tt','u','ul','var','video','wbr','xmp',
-
-// SVG elements
-'animate','animateMotion','animateTransform','circle','clipPath','cursor','defs','desc','discard','ellipse','feBlend','feColorMatrix','feComponentTransfer','feComposite','feConvolveMatrix','feDiffuseLighting','feDisplacementMap','feDistantLight','feDropShadow','feFlood','feFuncA','feFuncB','feFuncG','feFuncR','feGaussianBlur','feImage','feMerge','feMergeNode','feMorphology','feOffset','fePointLight','feSpecularLighting','feSpotLight','feTile','feTurbulence','filter','foreignObject','g','hatch','hatchpath','image','line','linearGradient','marker','mask','mesh','meshgradient','meshpatch','meshrow','metadata','mpath','path','pattern','polygon','polyline','radialGradient','rect','set','solidcolor','stop','svg','switch','symbol','text','textPath','tspan','unknown','use', 'view'
-].reduce(
-  (acc, i) => { acc[i] = new Selector(i); return acc },
-  {} as {[name: string]: Selector}
-) as {
-  [name in ElementNames]: Selector
-}
-
-export {tags}
